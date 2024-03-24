f08ce22eeb3586004c50cc882e7806aed3e263d7
Nurmuhammad Nazarov <78847300+neonazarov@users.noreply.github.com>
2024-03-24 06:36:24 +0500

    Update DELETE_Nurmuhammad_Nazarov.sql

--- DELETE_Nurmuhammad_Nazarov.sql
+++ DELETE_Nurmuhammad_Nazarov.sql
@@ -1,6 +1,6 @@
 SELECT film_id
 FROM film
-WHERE title = 'Forsage';
+WHERE title = 'Inception';
 
 DELETE FROM rental
 WHERE inventory_id IN (
@@ -14,8 +14,8 @@ WHERE film_id = X;
 
 SELECT customer_id
 FROM customer
-WHERE first_name = 'Jafar'
-AND last_name = 'Djurayev';
+WHERE first_name = 'Nurmuhammad'
+AND last_name = 'Nazarov';
 
 DELETE FROM payment WHERE customer_id = X;
 DELETE FROM rental WHERE customer_id = X;
